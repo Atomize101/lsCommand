df8be7c66fe463a0ca68be72cd068b6f42e0b290
Chris Patrick <atomize101@gmail.com>
2020-02-10 17:23:17 -0500

    Adding in chalk library

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const chalk = require('chalk');
 // const util = require('util');
 
 // Method 1
 
-// const lstat = util.promisify(fs.lstat);
+//const lstat = util.promisify(fs.lstat);
 
 fs.readdir(process.cwd(), async (err, filenames) => {
     if(err) {
         console.log(err);
     }
 
-    for(let filename of filenames) {
-        try {
-            const stats = await lstat(filename);
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
 
-        console.log(filename, stats.isFile());
-        } catch (err) {
-            console.log(err);        }
+    const allStats = await Promise.all(statPromises);
+
+    for(let stats of allStats) {
+        const index = allStats.indexOf(stats);
+        
+        if(stats.isFile()) {
+            console.log(filenames[index])
+        } else {
+            console.log(chalk.green(filenames[index]));
+        }
     }
 });
 
